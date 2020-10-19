a55a2d86869c5b2c8874710bc7576aee2461ba23
Seth Chart <seth.chart@protonmail.com>
2020-10-19 19:47:24 -0400

    Finished reading

--- school.py
+++ school.py
@@ -1,1 +1,18 @@
 class School:
+
+    def __init__(self, name, roster=dict()):
+        self.name = name
+        self.roster = roster
+
+    def add_student(self, fullName, gradeLevel):
+        student_list = self.roster.get(gradeLevel, [])
+        student_list.append(fullName)
+        self.roster[gradeLevel] = student_list
+
+    def grade(self, gradeLevel):
+        return self.roster[gradeLevel]
+
+    def sort_roster(self):
+        sorted_roster = { key: sorted(self.roster[key]) for key in
+                         sorted(self.roster.keys())}
+        return sorted_roster
